bd72faff095882b4cbbec3826a22c287baa38df7
Daniel <daniel.mota@sofex.com.mx>
2021-10-13 13:03:47 -0500

    try catch in app.js

--- public/app.js
+++ public/app.js
@@ -8,16 +8,20 @@ const app = new Vue({
     methods: {
         async createUrl() {
             console.log(this.url, this.slug)
-            const response = await fetch('/url', {
-                method: 'POST',
-                headers: {
-                    'content-type': 'application/json'
-                },
-                body: JSON.stringify({
-                    url: this.url,
-                    slug: this.slug
+            try {
+                const response = await fetch('/url', {
+                    method: 'POST',
+                    headers: {
+                        'content-type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        url: this.url,
+                        slug: this.slug
+                    })
                 })
-            })
+            } catch (error) {
+                console.log(error);
+            }
             console.log(response);
             this.created = await response.json();
         }
